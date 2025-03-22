65f6f5c7979ef27b906bbf75e0fce61927e3e2cb
Milodey <deysagar074@gmail.com>
2025-03-22 20:12:35 +0530

    refactor: comment out unused Script import in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { Providers } from "./providers";
-import Script from "next/script";
+// import Script from "next/script";
 // import Img from "next/image";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
