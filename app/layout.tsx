5c59edd9402e88b05448b4e33ff642211f5a2b4c
Milodey <deysagar074@gmail.com>
2025-03-22 18:16:05 +0530

    feat: add visitor tracking script to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { Providers } from "./providers";
+import Script from "next/script";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -20,8 +21,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Navbar />
           {children}
         </Providers>
-
-
+        <Script src="//mapmyvisitors.com/map.js?d=lTf7LYZCw0KHlsHgbd4S92SPLsnPT7MNwxO8zuOHAgU&cl=ffffff&w=a" strategy="afterInteractive" id="mapmyvisitors" />
+        {/* <script type="text/javascript" id="mapmyvisitors" src="//mapmyvisitors.com/map.js?d=lTf7LYZCw0KHlsHgbd4S92SPLsnPT7MNwxO8zuOHAgU&cl=ffffff&w=a"></script> */}
       </body>
     </html>
   );
